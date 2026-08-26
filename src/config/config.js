954d06c43b8d1c80e520eb90390f44c3b9300c82
David Jovani González Acevedo <jovaskein@gmail.com>
2026-08-26 16:43:19 -0600

    Add configuracion ssl

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
+
     development: {
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
@@ -25,6 +26,14 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: 'mysql'
+        dialect: 'mysql',
+
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
+
 };
--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,8 @@
 const { Sequelize } = require('sequelize');
+
 require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const databaseName =
     process.env.NODE_ENV === 'test'
@@ -15,7 +18,16 @@ const sequelize = new Sequelize(
         port: process.env.DB_PORT,
         dialect: 'mysql',
         logging: false,
-        timezone: '+00:00'
+        timezone: '+00:00',
+
+        ...(isProduction && {
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        })
     }
 );
 
